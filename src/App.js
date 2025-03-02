0df1e841af28f31ff7deb4cf36f6778205f2c260
zelisko <andrii.zelisko@gmail.com>
2025-03-03 01:20:51 +0200

    added data and created main components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React from 'react'
-import './App.scss'
-import Header from './components/Header'
-import Navigation from './components/Navigation'
-import AboutUs from './components/AboutUs'
-import Gallery from './components/Gallery'
-import Providers from './components/Providers'
-import Contacts from './components/Contacts'
-import Footer from './components/Footer'
+import React from 'react';
+import { Layout, Card, Col, Row, Descriptions } from 'antd';
+import store from './store/store.json';
+
+const { Header, Footer, Content } = Layout;
+
+const About = ({ about }) => (
+  <div>
+    <h1>{about.name}</h1>
+    <p>{about.data}</p>
+  </div>
+);
+
+const Manufactures = ({ manufacturers }) => {
+  const { name, data } = manufacturers;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <Row gutter={16}>
+        {data.map((item) => (
+          <Col span={8} key={item.key}>
+            <Card title={item.name} size="small" extra={<a href={item.site}>Site</a>}>
+              <p>{item.description}</p>
+              <p>{item.country}</p>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+const Services = ({ services }) => {
+  const { name, data } = services;
+  return (
+    <div>
+      <h2>{name}</h2>
+      {data.map((service, index) => (
+        <Card key={index} title={service.name} size="small">
+          <p>{service.description}</p>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+const Contacts = ({ contacts }) => {
+  const { name, data } = contacts;
+  return (
+    <div>
+      <Descriptions title={name}>
+        {data.map((item) => (
+          <Descriptions.Item key={item.key} label={item.label}>
+            {Array.isArray(item.children) ? item.children.join(', ') : item.children}
+          </Descriptions.Item>
+        ))}
+      </Descriptions>
+    </div>
+  );
+};
 
 function App() {
+  const { about, manufacturers, services, contacts } = store;
   return (
-    <div className="App">
-      <Header />
-      <Navigation />
-      <AboutUs />
-      <Gallery />
-      <Providers />
-      <Contacts />
-      <Footer />
-    </div>
-  )
+  <Layout>
+    <Header className="header">Header</Header>
+    <Content>
+      { about && <About className="about" about={about} ></About> }
+      { manufacturers && <Manufactures className="manufacturers" manufacturers={manufacturers} ></Manufactures> }
+      { services && <Services className="services" services={services} ></Services> }
+      { contacts && <Contacts className="contacts" contacts={contacts} ></Contacts> }
+    </Content>
+    <Footer className="footer">
+      @copyrigth 2025 by Avtosklo Zelena
+    </Footer>
+  </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer">
-      <div className="row">
-        <div className="container">© Created by Avtosklo Zelena</div>
-      </div>
-    </div>
-  )
-}
