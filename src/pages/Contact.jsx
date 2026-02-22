efafb91c65622bad7da67941258f68a0285ffc91
andrii-zelisko <andrii.zelisko@gmail.com>
2026-02-22 12:57:52 +0200

    updated Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,8 @@ import {Typography} from "@mui/material";
 export default function Contact() {
   return (
     <>
-      <Typography><strong>Телефон:</strong> +38 (032) 225 93 30</Typography>
-      <Typography><strong>Email:</strong> Avtosklo_Lviv@i.ua</Typography>
+      <Typography><strong>Телефон:</strong> <a href="tel:+380322259330">+38 (032) 225 93 30</a></Typography>
+      <Typography><strong>Email:</strong> <a href="mailto:Avtosklo_Lviv@i.ua">Avtosklo_Lviv@i.ua</a></Typography>
       <Typography><strong>Адреса:</strong> м. Львів, вул. Зелена, 251</Typography>
     </>
   )
